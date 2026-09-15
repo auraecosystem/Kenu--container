0f36c85f224a5614d663397aa6b3715c3b408f98
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-15 17:10:46 +0100

    Update and rename .sh to Newman/.run.sh

--- Newman/.run.sh
+++ Newman/.run.sh
@@ -5,3 +5,6 @@ git commit -m "Add action"
 git push
 git tag -a -m "Description of this release" v1
 git push --follow-tags
+npm install -g newman
+newman run my-collection.json -e dev-environment.json
+newman run https://www.postman.com/collections/<collection-id>
